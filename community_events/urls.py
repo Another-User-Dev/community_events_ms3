1f6794adbe51340557279d5b90261426bef77e4b
Another-User-Dev <michael.jobsearch.emails@gmail.com>
2025-09-14 23:09:04 +0100

    Install allauth

--- community_events/urls.py
+++ community_events/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from home_page import views as home_page
 
 urlpatterns = [
     path('home/', home_page.home, name='home'),
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
 ]
